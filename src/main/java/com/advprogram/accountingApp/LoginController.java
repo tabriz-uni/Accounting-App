fc01b65fb4ce2bc550f147097af650d123e2b14d
aliXshams <alishams.7107@gmail.com>
2020-05-08 19:15:11 +0430

    initial commit

--- src/main/java/com/advprogram/accountingApp/LoginController.java
+++ src/main/java/com/advprogram/accountingApp/LoginController.java
@@ -1,4 +1,114 @@
 package main.java.com.advprogram.accountingApp;
 
+import com.jfoenix.controls.JFXSlider;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.input.MouseEvent;
+
+import java.util.ArrayList;
+
+/** Controls the login screen */
 public class LoginController {
+    @FXML private TextField user;
+    @FXML private TextField password;
+    @FXML private Button loginButton;
+    @FXML private Label showPasswordHover;
+    @FXML private Button showPassword;
+    @FXML private JFXSlider type;
+    @FXML private Label InvalidCrdLbl;
+    private String revealedPassword;
+    private final String manager = "Ali Shams@1@1";
+
+    public void initialize() {
+
+    }
+    public void initManager(final LoginManager loginManager){
+        showPassword.addEventFilter(MouseEvent.MOUSE_PRESSED, e -> {
+            revealedPassword = password.getText();
+            password.clear();
+            password.setStyle("-fx-prompt-text-fill: white;");
+            password.setPromptText(revealedPassword);
+        });
+        showPassword.addEventFilter(MouseEvent.MOUSE_RELEASED, e -> {
+            password.setText(revealedPassword);
+            password.setStyle("-fx-prompt-text-fill: grey");
+            password.setPromptText("Password");
+        });
+        showPassword.addEventFilter(MouseEvent.MOUSE_ENTERED_TARGET, e -> {
+            showPasswordHover.setVisible(true);
+        });
+        showPassword.addEventFilter(MouseEvent.MOUSE_EXITED_TARGET, e -> {
+            showPasswordHover.setVisible(false);
+        });
+
+        loginButton.setOnAction(event -> {
+            if (type.getValue() == 0)
+            {
+                String Info;
+                Info = authorizeManager();
+                if (Info == null)
+                    InvalidCrdLbl.setVisible(true);
+                if (Info != null) {
+                    InvalidCrdLbl.setVisible(false);
+                    loginManager.authenticatedManager(Info);
+                }
+            }
+
+            else if (type.getValue() == 1)
+            {
+                String Info;
+                Info = authorizeStudent(loginManager.getStudentList());
+                if (Info == null)
+                    InvalidCrdLbl.setVisible(true);
+                if (Info != null) {
+                    InvalidCrdLbl.setVisible(false);
+                    loginManager.authenticatedStudent(Info);
+                }
+            }
+
+            else if (type.getValue() == 2)
+            {
+                String Info;
+                Info = authorizeProfessor(loginManager.getProfessorList());
+                if (Info == null)
+                    InvalidCrdLbl.setVisible(true);
+                if (Info != null) {
+                    InvalidCrdLbl.setVisible(false);
+                    loginManager.authenticatedProfessor(Info);
+                }
+            }
+        });
+    }
+
+    /**
+     * Check authorization credentials.
+     *
+     * If accepted, return a sessionID for the authorized session
+     * otherwise, return null.
+     */
+
+    private String authorizeProfessor(ArrayList<Professor> professorList) {
+        for (Professor professor : professorList) {
+            if (professor.getId().equals(user.getText()) && professor.getPass().equals(password.getText()))
+                return (professor.getName() + "@" + professor.getId());
+        }
+        return
+                null;
+    }
+
+    private String authorizeManager() {
+        if (user.getText().equals(manager.split("@")[1]) && password.getText().equals(manager.split("@")[2]))
+            return manager;
+        else
+            return null;
+    }
+
+    private String authorizeStudent(ArrayList<Student> studentList) {
+        for (Student student : studentList) {
+            if (student.getId().equals(user.getText()) && student.getPass().equals(password.getText()))
+                return (student.getName() + "@" + student.getId());
+        }
+        return
+                null;
+    }
 }
---
+++ src/main/java/com/advprogram/accountingApp/LoginManager.java
@@ -0,0 +1,135 @@
+package main.java.com.advprogram.accountingApp;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.*;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+
+/** Manages control flow for logins */
+public class LoginManager extends Node {
+    private final String[] urls = {"src/main/resources/files/professorInfo.txt",
+            "src/main/resources/files/studentInfo.txt", "src/main/resources/files/courseInfo.txt"};
+    protected ArrayList<Professor> professorList = new ArrayList<>();
+    protected ArrayList<Student> studentList = new ArrayList<>();
+    protected ArrayList<Course> courseList = new ArrayList<>();
+    private final Scene scene;
+
+    public ArrayList<Professor> getProfessorList() { return professorList; }
+
+    public ArrayList<Student> getStudentList() { return studentList; }
+
+    public ArrayList<Course> getCourseList() { return courseList; }
+
+    public LoginManager(Scene scene) {
+        this.scene = scene;
+    }
+
+    public void createArrayLists() throws FileNotFoundException {
+        Scanner professorSc = new Scanner(new File(urls[0]));
+        Scanner studentSc = new Scanner(new File(urls[1]));
+        Scanner courseSc = new Scanner(new File(urls[2]));
+        String line;
+        while (professorSc.hasNextLine()) {
+            line = professorSc.nextLine();
+            Professor professor = new Professor();
+            professor.setName(line.split("@")[0]);
+            professor.setId(line.split("@")[1]);
+            professor.setPass(line.split("@")[2]);
+            professorList.add(professor);
+        }
+        while (studentSc.hasNextLine()) {
+            line = studentSc.nextLine();
+            Student student = new Student();
+            student.setName(line.split("@")[0]);
+            student.setId(line.split("@")[1]);
+            student.setPass(line.split("@")[2]);
+            student.setSex(line.split("@")[3]);
+            studentList.add(student);
+        }
+        while (courseSc.hasNextLine()) {
+            line = courseSc.nextLine();
+            Course course = new Course();
+            course.setCourseName(line.split("@")[0]);
+            course.setCourseId(line.split("@")[1]);
+            courseList.add(course);
+        }
+    }
+
+    /**
+     * Callback method invoked to notify that a user has been authenticated.
+     * Will show the main application screen.
+     */
+    public void authenticatedManager(String sessionID) {
+        showManagerView(sessionID);
+    }
+
+    public void authenticatedProfessor(String sessionID) { showProfessorView(sessionID); }
+
+    public void authenticatedStudent(String sessionID) { showStudentView(sessionID); }
+
+    /**
+     * Callback method invoked to notify that a user has logged out of the main application.
+     * Will show the login application screen.
+     */
+    public void logout() {
+        showLoginScreen();
+    }
+
+    public void showLoginScreen() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("loginView.fxml")
+            );
+            scene.setRoot(loader.load());
+            LoginController controller =
+                    loader.<LoginController>getController();
+            controller.initManager(this);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void showManagerView(String sessionID) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("managerView.fxml")
+            );
+            scene.setRoot(loader.load());
+            ManagerController controller =
+                    loader.<ManagerController>getController();
+            controller.initSessionID(this, sessionID);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void showProfessorView(String sessionID) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("professorView.fxml")
+            );
+            scene.setRoot(loader.load());
+            ProfessorController controller =
+                    loader.getController();
+            controller.initSessionID(this, sessionID);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private void showStudentView(String sessionID) {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource("studentView.fxml")
+            );
+            scene.setRoot(loader.load());
+            StudentController controller =
+                    loader.getController();
+            controller.initSessionID(this, sessionID);
+        } catch (IOException ex) {
+            Logger.getLogger(LoginManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
