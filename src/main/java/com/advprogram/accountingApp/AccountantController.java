2d5569de2609faa5d446f221dc7cdd0e1e34ad45
nuxanor <amirmjj79@gmail.com>
2020-06-06 22:01:58 +0430

    .

--- src/main/java/com/advprogram/accountingApp/AccountantController.java
+++ src/main/java/com/advprogram/accountingApp/AccountantController.java
@@ -17,7 +17,7 @@ public class AccountantController {
     @FXML
     private Label lblName, lblAccNo;
     @FXML
-    private AnchorPane profilePage, professorsPage, studentsPage, coursesPage, confirmPage;
+    private AnchorPane profilePage, PersonnelTablePage, EmpAddPage, EmployeeEditPage, confirmPage;
     @FXML
     private TableView PersonnelTable;
     @FXML
@@ -26,123 +26,107 @@ public class AccountantController {
     private TextField txtFNameAdd, txtLNameAdd, txtIDAdd, txtOffspringAdd, txtTitleAdd, txtBaseSalaryAdd;
     @FXML
     private Button btnAddEmp;
+
+
+    public void initialize() { }
+
+    public void initSessionID(final LoginManager loginManager, Accountant accountant) {
+        lblName.setText(accountant.getFirstName()+" "+accountant.getLastName());
+        lblAccNo.setText(String.valueOf(accountant.getId()));
+
+        /* Event handlers for side menu items */
+        btnProfilePage.setOnAction(event -> {
+            btnProfilePage.setStyle(magenta());
+            VboxBtnAddEmp.setStyle(purple());
+            btnEditEmployee.setStyle(purple());
+            btnPrsnlInfo.setStyle(purple());
+            btnExit.setStyle(purple());
+            btnLogout.setStyle(purple());
+            profilePage.setVisible(true);
+            EmpAddPage.setVisible(false);
+            EmployeeEditPage.setVisible(false);
+            PersonnelTablePage.setVisible(false);
+            confirmPage.setVisible(false);
+        });
+
+        btnPrsnlInfo.setOnAction(event -> {
+            btnProfilePage.setStyle(purple());
+            VboxBtnAddEmp.setStyle(purple());
+            btnEditEmployee.setStyle(purple());
+            btnPrsnlInfo.setStyle(magenta());
+            btnExit.setStyle(purple());
+            btnLogout.setStyle(purple());
+            profilePage.setVisible(false);
+            EmpAddPage.setVisible(false);
+            EmployeeEditPage.setVisible(false);
+            PersonnelTablePage.setVisible(true);
+            confirmPage.setVisible(false);
+        });
+
+        VboxBtnAddEmp.setOnAction(event -> {
+            btnProfilePage.setStyle(purple());
+            VboxBtnAddEmp.setStyle(magenta());
+            btnEditEmployee.setStyle(purple());
+            btnPrsnlInfo.setStyle(purple());
+            btnExit.setStyle(purple());
+            btnLogout.setStyle(purple());
+            profilePage.setVisible(false);
+            EmpAddPage.setVisible(true);
+            EmployeeEditPage.setVisible(false);
+            PersonnelTablePage.setVisible(false);
+            confirmPage.setVisible(false);
+        });
+
+        btnEditEmployee.setOnAction(event -> {
+            btnProfilePage.setStyle(purple());
+            VboxBtnAddEmp.setStyle(purple());
+            btnEditEmployee.setStyle(magenta());
+            btnPrsnlInfo.setStyle(purple());
+            btnExit.setStyle(purple());
+            btnLogout.setStyle(purple());
+            profilePage.setVisible(false);
+            EmpAddPage.setVisible(false);
+            EmployeeEditPage.setVisible(true);
+            PersonnelTablePage.setVisible(false);
+            confirmPage.setVisible(false);
+        });
+
+
+        btnLogout.setOnAction(event -> {
+            btnProfilePage.setStyle(purple());
+            VboxBtnAddEmp.setStyle(purple());
+            btnEditEmployee.setStyle(purple());
+            btnPrsnlInfo.setStyle(purple());
+
+            btnExit.setStyle(purple());
+            btnLogout.setStyle(magenta());
+            int input = JOptionPane.showConfirmDialog(null, "Are you sure?", "Log Out",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (input == 0) loginManager.logout();
+        });
+
+        btnExit.setOnAction(event -> {
+            btnProfilePage.setStyle(purple());
+            VboxBtnAddEmp.setStyle(purple());
+            btnEditEmployee.setStyle(purple());
+            btnPrsnlInfo.setStyle(purple());
+
+            btnLogout.setStyle(purple());
+            btnExit.setStyle(magenta());
+            Stage stage = (Stage) btnExit.getScene().getWindow();
+            int input = JOptionPane.showConfirmDialog(null, "Are you sure?", "Log Out",
+                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+            if (input == 0) stage.close();;
+        });
+//        End of side menu handlers
+
+//        End
+    }
+
+    private String purple() {
+        return "-fx-background-color: #673ab7; -fx-background-radius: 70";
+    }
+    private String magenta() {
+        return "-fx-background-color: #97117A; -fx-background-radius: 70";
+    }
 }
-
-//    public void initialize() { }
-
-//    public void initSessionID(final LoginManager loginManager, Accountant accountant) {
-//        lblName.setText(accountant.getFirstName()+" "+accountant.getLastName());
-//        lblAccNo.setText(String.valueOf(accountant.getId()));
-//
-//        /* Event handlers for side menu items */
-//        btnProfilePage.setOnAction(event -> {
-//            btnProfilePage.setStyle(magenta());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(purple());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            profilePage.setVisible(true);
-//            professorsPage.setVisible(false);
-//            studentsPage.setVisible(false);
-//            coursesPage.setVisible(false);
-//            confirmPage.setVisible(false);
-//        });
-//
-//        btnProfessorsPage.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(magenta());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(purple());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            profilePage.setVisible(false);
-//            professorsPage.setVisible(true);
-//            studentsPage.setVisible(false);
-//            coursesPage.setVisible(false);
-//            confirmPage.setVisible(false);
-//        });
-//
-//        btnStudentsPage.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(magenta());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(purple());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            profilePage.setVisible(false);
-//            professorsPage.setVisible(false);
-//            studentsPage.setVisible(true);
-//            coursesPage.setVisible(false);
-//            confirmPage.setVisible(false);
-//        });
-//
-//        btnCoursesPage.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(magenta());
-//            btnConfirmPage.setStyle(purple());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            profilePage.setVisible(false);
-//            professorsPage.setVisible(false);
-//            studentsPage.setVisible(false);
-//            coursesPage.setVisible(true);
-//            confirmPage.setVisible(false);
-//        });
-//        btnConfirmPage.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(magenta());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            profilePage.setVisible(false);
-//            professorsPage.setVisible(false);
-//            studentsPage.setVisible(false);
-//            coursesPage.setVisible(false);
-//            confirmPage.setVisible(true);
-//        });
-//        btnLogout.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(purple());
-//            btnExit.setStyle(purple());
-//            btnLogout.setStyle(magenta());
-//            int input = JOptionPane.showConfirmDialog(null, "Are you sure?", "Log Out",
-//                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-//            if (input == 0) loginManager.logout();
-//        });
-//
-//        btnExit.setOnAction(event -> {
-//            btnProfilePage.setStyle(purple());
-//            btnProfessorsPage.setStyle(purple());
-//            btnStudentsPage.setStyle(purple());
-//            btnCoursesPage.setStyle(purple());
-//            btnConfirmPage.setStyle(purple());
-//            btnLogout.setStyle(purple());
-//            btnExit.setStyle(magenta());
-//            Stage stage = (Stage) btnExit.getScene().getWindow();
-//            int input = JOptionPane.showConfirmDialog(null, "Are you sure?", "Log Out",
-//                    JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-//            if (input == 0) stage.close();;
-//        });
-////        End of side menu handlers
-//
-////        End
-//    }
-
-//    private String purple() {
-//        return "-fx-background-color: #673ab7; -fx-background-radius: 70";
-//    }
-//    private String magenta() {
-//        return "-fx-background-color: #97117A; -fx-background-radius: 70";
-//    }
-//}
