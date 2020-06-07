3d777cb62c5a9ea963a6884bd8517754fff90e28
nuxanor <amirmjj79@gmail.com>
2020-06-07 23:14:33 +0430

    .

--- src/main/java/com/advprogram/accountingApp/AccountantController.java
+++ src/main/java/com/advprogram/accountingApp/AccountantController.java
@@ -23,7 +23,15 @@ public class AccountantController {
     private JFXButton btnProfilePage, vboxBtnAddEmp, btnEditEmployee,
             btnSubmitChanges, btnPrsnlInfo, btnLogout, btnExit, btnSearchID;
     @FXML
-    private Label lblName, lblAccNo;
+    private Label lblName, lblAccNo,
+    emptyfeildFN,
+            emptyfeildLN,
+    emptyfeildNI,
+            emptyfeildNOO,
+    emptyfeildWE,
+            emptyfeildT,
+    emptyfeildBS,
+            empexstswarning  ;
     @FXML
     private AnchorPane profilePage, personnelTablePage, empAddPage, employeeEditPage, confirmPage;
     @FXML
